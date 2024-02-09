74520d0464663e7e1e544fff7e53f88c219be484
Muhammad Ammaad Tehseen <ammadtehseenkhan@gmail.com>
2024-02-09 20:48:23 +0500

    adding data to database

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,11 @@ connectToMongo()
 
 const app = express()
 const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+//avalable end points
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
