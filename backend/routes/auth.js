7d9c9dc2dfbbb7541943927fb4f34d2b1868cac5
Muhammad Ammaad Tehseen <ammadtehseenkhan@gmail.com>
2024-02-10 11:03:39 +0500

    10-2-24

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const User = require('../models/User')
 
-router.get('/', (req, res) => {
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password').isLength({ min: 8 })
+], (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json(({ errors: error.array() }))
+    }
 
+    //create auser using post
     console.log(req.body);
     const user = User(req.body);
     user.save();
