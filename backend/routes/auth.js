0ce27c4530dd6afcdb74c5130491afdce5bd3f57
Muhammad Ammaad Tehseen <ammadtehseenkhan@gmail.com>
2024-02-27 12:37:29 +0500

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,13 +7,14 @@ const jwt = require('jsonwebtoken');
 
 const jwT_Secret = "ammadisagoodb$oy"
 
-// creating a route
+// creating a route using post :/api/auth/createUser
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password').isLength({ min: 8 })
 ], async (req, res) => {
     const error = validationResult(req);
+    //checking for errors
     if (!error.isEmpty()) {
         return res.status(400).json(({ errors: error.array() }))
     }
@@ -47,5 +48,49 @@ router.post('/createuser', [
         console.error(error.message);
         res.status(500).send("Some error occured")
     }
+})
+// creating a route using post :/api/auth/login
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'password cannot be blank').exists()
+], async (req, res) => {
+    const error = validationResult(req);
+
+    //check for error
+    if (!error.isEmpty()) {
+        return res.status(400).json(({ errors: error.array() }))
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: 'use right credentials' })
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: 'use right credentials' })
+
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, jwT_Secret);
+        res.json({ authToken })
+    }
+    catch (err) {
+        console.error(error.message);
+        res.status(500).send("Some error occured")
+    }
+})
+
+// creating a route  for loging using post :/api/auth/getUser
+router.post('/getUser', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'password cannot be blank').exists()
+], async (req, res) => {
+
+
 })
 module.exports = router
