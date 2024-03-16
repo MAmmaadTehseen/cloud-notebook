0181002154f71a33248fcf99c48ec91ba5d5d92d
Muhammad Ammaad Tehseen <ammadtehseenkhan@gmail.com>
2024-03-16 21:06:28 +0500

    implemented Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Home from './Components/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
